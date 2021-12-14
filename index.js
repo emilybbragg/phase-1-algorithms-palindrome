f6d369026f93613ffd5e5abc3024e492c7537fcc
Emily <emilybribragg@gmail.com>
2021-12-14 16:42:42 -0500

    commit tree 7f254dd83123f237c8883fed86a6a2d0e4d92b73

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
+//SOLUTION 1
+//creates function that reverses word, to be used in isPalindrome function
+function reverse(word){
+  return word.split("").reverse().join("")
+}
+
+
+
 function isPalindrome(word) {
   // Write your algorithm here
+  //reverse input string
+const reversedWord = reverse(word)
+  if (word === reversedWord) {
+    return true
+  } else {
+    return false
+  }
+}
+//OR
+function isPalindrome(word) {
+const reversedWord = reverse(word)
+return word === reversedWord
 }
 
 /* 
   Add your pseudocode here
+
+  if input is same as reverse
+    return tree
+  else
+    return false
 */
+
+
+//SOLUTION 2
+
+
+
 
 /*
   Add written explanation of your solution here
